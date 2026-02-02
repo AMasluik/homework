03b153e3f1c3dd84a334a6b60f362aeb86bfcedd
AMasluik <sasha.masluik@gmail.com>
2026-02-02 22:17:52 +0100

    домашка 5.2 modificated calculator

--- Homework03.py
+++ Homework03.py
@@ -1,17 +1,25 @@
 print("Calculator")
-a = float(input("Enter the first number : "))
-b = str(input("Enter the sing : "))
-c = float(input("Enter the second number : "))
 
-if b == "+":
-    print(a + c)
-elif b == "-":
-    print(a - c)
-elif b == "*":
-    print(a * c)
-elif b == "/" and c == 0:
-    print("division by zero")
-elif  b == "/":
-    print(a / c)
-else:
-    print("Please enter a valid input")
+while True:
+    a = float(input("Enter the first number : "))
+    b = str(input("Enter the sing : "))
+    c = float(input("Enter the second number : "))
+
+    if b == "+":
+        print(a + c)
+    elif b == "-":
+        print(a - c)
+    elif b == "*":
+        print(a * c)
+    elif b == "/" and c == 0:
+        print("division by zero")
+    elif b == "/":
+        print(a / c)
+    else:
+        print("Please enter a valid input")
+
+    x = input("Press yes or no to continue ")
+    if x == "no":
+        exit()
+    elif x != "yes":
+        print("Please enter a valid input")
