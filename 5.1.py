5001168ea4396e959a7b73dea34ba2e1ce09a4b4
AMasluik <sasha.masluik@gmail.com>
2026-02-02 21:54:41 +0100

    домашка 5.1 проблема була в лишней проверке на количество  нижних подчеркиваний

--- 5.1.py
+++ 5.1.py
@@ -3,26 +3,20 @@ import string
 
 variable = input("Enter a variable: ")
 
-if not variable:
+if variable[0].isdigit():
     print(False)
+
+elif any(char.isupper() for char in variable):
+    print(False)
+
+elif ' ' in variable:
+    print(False)
+
+elif any(char in string.punctuation.replace('_', '') for char in variable):
+    print(False)
+
+elif variable in keyword.kwlist:
+    print(False)
+
 else:
-    if variable[0].isdigit():
-        print(False)
-
-
-    elif any(char.isupper() for char in variable):
-        print(False)
-
-    elif ' ' in variable:
-        print(False)
-
-
-    elif any(char in string.punctuation.replace('_', '') for char in variable):
-        print(False)
-
-    elif variable in keyword.kwlist:
-        print(False)
-
-    else:
-        print(True)
     print("Thanks for valid input")
