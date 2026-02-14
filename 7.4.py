a95a0582ccb23f77c1219adb5c9c67922c79ff53
AMasluik <sasha.masluik@gmail.com>
2026-02-14 16:20:11 +0100

    7.4.py

--- 7.4.py
+++ 7.4.py
@@ -2,7 +2,7 @@ def common_elements():
     return set(range(0, 100, 3)) & set(range(0, 100, 5))
 
 result = common_elements()
-#print(f"Result: {result}") ЕСЛИ НУЖНО ТО ВОТ
-assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+#print(f"Result: {result}") если нужно то вот 
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}#но я думаю что у нас есть проверка и принт не особо имеет смысл 
 
 
