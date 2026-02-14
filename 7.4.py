7b2e9cb63cc82873b7a3bcdf22ab134634b2a1ec
AMasluik <sasha.masluik@gmail.com>
2026-02-14 16:03:03 +0100

    домашка 7.4

--- 7.4.py
+++ 7.4.py
@@ -2,7 +2,7 @@ def common_elements():
     return set(range(0, 100, 3)) & set(range(0, 100, 5))
 
 result = common_elements()
-print(f"Result: {result}")
+#print(f"Result: {result}") ЕСЛИ НУЖНО ТО ВОТ
 assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
 
 
