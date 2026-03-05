02f611685ce0a65c740201443a4a3e344774decc
AMasluik <sasha.masluik@gmail.com>
2026-03-05 18:03:35 +0100

    домашка 12.1

---
+++ 12.1.py
@@ -0,0 +1,24 @@
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with open(html_file, 'r', encoding='utf-8') as f:
+        html = f.read()
+
+    text = ''
+    inside_tag = False
+
+    for char in html:
+        if char == '<':
+            inside_tag = True
+        elif char == '>':
+            inside_tag = False
+        elif not inside_tag:
+            text += char
+
+    clean_lines = [line.strip() for line in text.split('\n') if line.strip()]
+    result = '\n'.join(clean_lines)
+
+    with open(result_file, 'w', encoding='utf-8') as f:
+        f.write(result)
+
+
+if __name__ == '__main__':
+    delete_html_tags('draft.html')
